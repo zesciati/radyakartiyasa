8d37be7c400e5a9ae35a1923c360813251504954
zesciati <zakyabiyyu2003@gmail.com>
2025-02-04 15:42:18 +0800

    mengubah node jadi cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
 });
