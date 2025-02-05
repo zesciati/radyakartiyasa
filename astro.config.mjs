8fd474365ef63d8c8c40cccfd5105f27c046ba16
zesciati <zakyabiyyu2003@gmail.com>
2025-02-05 10:43:58 +0800

    memasukkan konten radyakartiyasa ke folder dinamis

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
