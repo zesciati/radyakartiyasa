acb9fe4b0404b9b30ab548ce7cd6c6c6ea175d06
ShabrinaHazratiB <shabrina.bahirah07@gmail.com>
2025-02-22 22:04:37 +0700

    form component

--- src/pages/api/form/submit.js
+++ src/pages/api/form/submit.js
@@ -1,4 +1,4 @@
-// import { function } from "./middleware.js";
+import { onRequest } from "./middleware.js";
 import { config, secretKey } from "./utilsSecret.js";
 
 // document.addEventListener("DOMContentLoaded", function () {
@@ -13,6 +13,7 @@ import { config, secretKey } from "./utilsSecret.js";
 //             const phone = form.phone.value.trim();
 //             const message = form.message.value.trim();
 
+//             // Validasi
 //             if (!name || !email || !profession || !phone || !message) {
 //                 alert("Kolom harus diisi!");
 //                 return;
@@ -26,10 +27,14 @@ import { config, secretKey } from "./utilsSecret.js";
 //     }
 // });
 
+// Menangani request POST
+
 export async function POST(ctx) {
     const data = await ctx.request.formData();
     const turnstileToken = data.get('cf-turnstile-response');
+    // const turnstileToken = document.getElementById('cf-turnstile-response').value;
 
+    // Verif token turnstile
     const turnstile = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
         method: 'POST',
         headers: {
@@ -42,16 +47,21 @@ export async function POST(ctx) {
     });
 
     console.dir(turnstile.status);
-    if (turnstile.ok) {
-     const directus = await fetch('https://backenddirectus.madebybagus.xyz/items/contact', {
+
+    if (!turnstile.ok) {
+        return new Response(JSON.stringify({message:"Verification Failed"},{status:400}));
+    }
+    console.log("Turnstile success");
+
+    // Kirim data ke Directus
+    const directus = await fetch('https://backenddirectus.madebybagus.xyz/items/contact', {
+        method: "POST",
         headers: {
             Authorization: config.Authorization,
             "CF-Access-Client-Id": config.headers["CF-Access-Client-Id"],
             "CF-Access-Client-Secret": config.headers["CF-Access-Client-Secret"],
             "Content-Type": "application/json",
         },
-        method: "POST",
-
         body: JSON.stringify({
             name: data.get("name"),
             email: data.get("email"),
@@ -61,7 +71,41 @@ export async function POST(ctx) {
         })
     });
 
-    return directus;
+    if(!directus.ok) {
+        return new Response(JSON.stringify({message:"Failed to submit form"},{status:400}));
     }
-    return new Response(JSON.stringify({message:"Failure"},{status:400}));
+    console.log("Directus success");
+
+    return new Response(JSON.stringify({message:"Form submitted"},{status:200}));
 }
+// Menangani request POST dari form
+// export async function POST(ctx){ 
+//     const payload = await ctx.request.json();
+
+//     console.log(payload);
+
+//     const req = await fetch("/api/form/submit",{
+//         method:"POST",
+//         headers:{
+//             "Content-Type":"application/json"
+//         },
+//         body:JSON.stringify({
+//             name:payload.name,
+//             email:payload.email,
+//             profession:payload.profession,
+//             phone:payload.phone,
+//             message:payload.message,
+//             mode:"session"
+//         })
+//     });
+
+//     console.log("response headers:", req.headers);
+//     const headers = new Headers();
+
+//     return new Response(
+//         req.statusText, {
+//             status: req.status,
+//             headers:headers
+//         }
+//     );
+// }
